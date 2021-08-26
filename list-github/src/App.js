e0fccfc605b12512721f700a81b7bbb5b306fe50
Rubito <rubitojogja@gmail.com>
2021-08-26 23:11:12 +0700

    first commit

--- list-github/src/App.js
+++ list-github/src/App.js
@@ -3,13 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       
-      
+    <div>
+        <h4>
+          List Repository Github
+        </h4>
     </div>
   );
 }
